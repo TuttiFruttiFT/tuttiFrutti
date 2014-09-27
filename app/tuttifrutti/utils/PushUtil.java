b0aa612ebd7bb8b7f48f4b129587e57851109bdc
ruben.fanego <ruben.fanego@mercadolibre.com>
2014-09-26 23:34:25 -0300

    changing push services

--- app/tuttifrutti/utils/PushUtil.java
+++ app/tuttifrutti/utils/PushUtil.java
@@ -1,6 +1,8 @@
 package tuttifrutti.utils;
 
+import static org.apache.commons.lang3.StringUtils.join;
 import static play.libs.F.Promise.promise;
+import static play.libs.Json.newObject;
 import static tuttifrutti.utils.PushType.MATCH_RESULT;
 import static tuttifrutti.utils.PushType.PRIVATE_MATCH_READY;
 import static tuttifrutti.utils.PushType.PUBLIC_MATCH_READY;
@@ -61,7 +63,7 @@ public class PushUtil {
 
 	public void roundResult(Match match, Integer roundNumber) {
 		promise(() -> {
-			ObjectNode json = Json.newObject().put("type", ROUND_RESULT.toString()).put("match_id", match.getId().toString())
+			ObjectNode json = newObject().put("type", ROUND_RESULT.toString()).put("match_id", match.getId().toString())
 											  .put("round_number", roundNumber);
 			for(String playerId : match.playerIds()){
 				sendPushwooshMessage(json.put("player_id", playerId).toString(), Arrays.asList(playerId));
@@ -79,14 +81,14 @@ public class PushUtil {
 	}
 	
 	private void sendPushwooshMessage(String jsonData,List<String> playerIds){
-		ArrayNode notifications = Json.newObject().arrayNode();
-		notifications.add(Json.newObject().put("send_date", "now").put("data", jsonData));
-		JsonNode requestBody = Json.newObject().put("auth", AUTH_TOKEN).put("devices_filter",playerPushTags(playerIds))
+		ArrayNode notifications = newObject().arrayNode();
+		notifications.add(newObject().put("send_date", "now").put("data", jsonData));
+		JsonNode requestBody = newObject().put("auth", AUTH_TOKEN).put("devices_filter",playerPushTags(playerIds))
 											   .set("notifications", notifications);
-		JsonNode request = Json.newObject().set("request", requestBody);
+		JsonNode request = newObject().set("request", requestBody);
 		for(int i = 0;i < RETRIES_NUMBER;i++){			
 			WSResponse r = WS.url(PUSHWOOSH_SERVICE_BASE_URL + "createTargetedMessage").setContentType("application/json")
-					.post(request.toString())
+					.post(request)
 					.get(5000L);
 			if(r.getStatus() == Status.OK){
 				JsonNode response = r.asJson();
@@ -105,39 +107,39 @@ public class PushUtil {
 	}
 
 	public void registerDevice(String pushToken,String hardwareId,String language){
-		ObjectNode registerJsonBody = Json.newObject();
+		ObjectNode registerJsonBody = newObject();
 		registerJsonBody.put("application", APPLICATION_CODE);
 		registerJsonBody.put("push_token", pushToken);
 		registerJsonBody.put("language", language);
 		registerJsonBody.put("hwid", hardwareId);
 		registerJsonBody.put("device_type", ANDROID_DEVICE_TYPE);
-		JsonNode registerJson = Json.newObject().put("register", registerJsonBody);
+		JsonNode registerJson = newObject().put("register", registerJsonBody);
 		WSResponse r = WS.url(PUSHWOOSH_SERVICE_BASE_URL + "registerDevice").setContentType("application/json")
-				 .post(registerJson.asText())
+				 .post(registerJson)
 				 .get(5000L);
 		
 		processPushResponse(r, "registering device with hwid " + hardwareId);
 	}
 	
 	public void setTag(String hardwareId,String playerId){
-		ObjectNode tagJson = Json.newObject().put("player_id", playerId);
-		ObjectNode jsonBody = Json.newObject();
+		ObjectNode tagJson = newObject().put("player_id", playerId);
+		ObjectNode jsonBody = newObject();
 		jsonBody.put("application", APPLICATION_CODE);
 		jsonBody.put("hwid", hardwareId);
 		jsonBody.put("tags", tagJson);
 		WSResponse r = WS.url(PUSHWOOSH_SERVICE_BASE_URL + "setTags").setContentType("application/json")
-				 .post(jsonBody.asText())
+				 .post(jsonBody)
 				 .get(5000L);
 		
 		processPushResponse(r, "set tag for player " + playerId);
 	}
 
 	public void unRegisterDevice(String hardwareId) {
-		ObjectNode jsonBody = Json.newObject();
+		ObjectNode jsonBody = newObject();
 		jsonBody.put("application", APPLICATION_CODE);
 		jsonBody.put("hwid", hardwareId);
 		WSResponse r = WS.url(PUSHWOOSH_SERVICE_BASE_URL + "unregisterDevice").setContentType("application/json")
-				 .post(jsonBody.asText())
+				 .post(jsonBody)
 				 .get(5000L);
 		
 		processPushResponse(r, "unregistering device with hwid " + hardwareId);
@@ -165,11 +167,11 @@ public class PushUtil {
 		for(String playerId : playerIds){
 			tags.add(String.format("T(\"playerId\", EQ, \"%s\")", playerId));
 		}
-		return StringUtils.join(tags," + ");
+		return join(tags," + ");
 	}
 
 	private void sendMessageTo(List<String> playerIds, Match match,PushType pushType) {
-		ObjectNode json = Json.newObject().put("type", pushType.toString()).put("match_id", match.getId().toString());
+		ObjectNode json = newObject().put("type", pushType.toString()).put("match_id", match.getId().toString());
 		for(String playerId : playerIds){
 			sendPushwooshMessage(json.put("player_id", playerId).toString(), Arrays.asList(playerId));
 		}
