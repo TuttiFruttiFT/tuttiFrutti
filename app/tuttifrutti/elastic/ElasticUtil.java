c6a26bb63a0b28885d2e9e832f19a7603024e116
ruben.fanego <ruben.fanego@mercadolibre.com>
2014-11-01 19:13:41 -0300

    fixing elastic query for alphabet job

--- app/tuttifrutti/elastic/ElasticUtil.java
+++ app/tuttifrutti/elastic/ElasticUtil.java
@@ -181,7 +181,7 @@ public class ElasticUtil {
 
 	public Map<String, Boolean> availableLettersForCategory(String categoryName) {
 		Map<String,Boolean> availableLetters = new HashMap<String, Boolean>(); 
-		TermsBuilder termsBuilder = AggregationBuilders.terms("group_by_letter").field(LETTER_FIELD);
+		TermsBuilder termsBuilder = AggregationBuilders.terms("group_by_letter").field(LETTER_FIELD).size(0);
 		SearchResponse searchResponse = elasticSearchClient.prepareSearch(CATEGORIES_INDEX).setTypes(categoryName).addAggregation(termsBuilder).setSize(0).execute().actionGet();
 		Aggregations aggregations = searchResponse.getAggregations();
 		Terms terms = aggregations.get("group_by_letter");
