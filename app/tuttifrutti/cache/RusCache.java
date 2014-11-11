bea75664c34bbe98fbe90187998396e74465e7ae
ruben.fanego <ruben.fanego@mercadolibre.com>
2014-11-11 16:02:22 -0300

    changing rus cache

--- app/tuttifrutti/cache/RusCache.java
+++ app/tuttifrutti/cache/RusCache.java
@@ -2,6 +2,7 @@ package tuttifrutti.cache;
 
 import org.springframework.stereotype.Component;
 
+import play.Logger;
 import play.cache.Cache;
 import tuttifrutti.models.enums.PowerUpType;
 import tuttifrutti.utils.ConfigurationAccessor;
@@ -15,10 +16,12 @@ public class RusCache {
 	public static final String PREFIX = "RUS_";
 	
 	public void loadRus(PowerUpType powerUp) {
+		Logger.info("Loading Rus Cache " + PREFIX + powerUp.toString() + " value "
+				+ ConfigurationAccessor.i("rus.powerUp." + powerUp.toString()));
 		Cache.set(PREFIX + powerUp.toString(), ConfigurationAccessor.i("rus.powerUp." + powerUp.toString()));
 	}
 
 	public int rusFor(PowerUpType powerUp){
-		return (int)Cache.get(PREFIX + powerUp.toString());
+		return (Integer)Cache.get(PREFIX + powerUp.toString());
 	}
 }
